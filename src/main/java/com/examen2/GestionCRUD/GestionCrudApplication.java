0c38220ebf803cf442927879f85b7deea198e3ab
majovp-25 <majopv.epn25@gmail.com>
2026-01-13 22:29:15 -0500

    Initial commit

---
+++ src/main/java/com/examen2/GestionCRUD/GestionCrudApplication.java
@@ -0,0 +1,13 @@
+package com.examen2.GestionCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCrudApplication.class, args);
+	}
+
+}
